d9e2636aa4f0770a08bc894f6d8f6d6cb73d9901
ML-Mounika <159158311+ML-Mounika@users.noreply.github.com>
2024-07-03 00:00:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform {
-    
-	backend "s3" {
- bucket = "my-tf-test-bucket9234"
-    key    = "MLMounikaTest1/awsDemo/terraform.tfstate"
-    region = "us-east-1"
-	}
-}
 provider "aws" {
 region = "us-east-1"
 }
